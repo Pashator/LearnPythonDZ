007d87d5a38526efc562508ee2a98861837afd36
Pashator <karkamish35@gmail.com>
2026-05-22 18:57:10 +0200

    Create 01_os_info.py

---
+++ extra_homework/hw1/01_os_info.py
@@ -0,0 +1,13 @@
+import sys
+import platform
+
+os_name = platform.system()
+
+python_version = sys.version
+
+info_line = f"OS info is {os_name} Python version is {python_version}"
+
+with open("os_info.txt", "w", encoding="utf-8") as file:
+    file.write(info_line)
+
+print("Файл os_info.txt успешно создан!")
