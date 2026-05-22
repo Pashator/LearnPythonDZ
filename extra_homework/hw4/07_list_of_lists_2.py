1d9872276963fbe89994ef3ecdeb00a447ce9227
Pashator <karkamish35@gmail.com>
2026-05-22 21:18:12 +0200

    Create 07_list_of_lists_2.py

---
+++ extra_homework/hw4/07_list_of_lists_2.py
@@ -0,0 +1,7 @@
+# ЮНУСОВ ПАВЕЛ ИИАД 1
+
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+flat_list = [num for block in nice_list for group in block for num in group]
+
+print(flat_list)
